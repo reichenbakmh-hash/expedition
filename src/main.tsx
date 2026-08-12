0d48fc8b82ee638ddecd680a63601642fb1638e7
reichenbakmh-hash <reichenbakmh@gmail.com>
2026-08-12 18:54:56 +0300

    Refactor main.tsx by cleaning up imports
    Removed unused imports and service worker registration.

--- src/main.tsx
+++ src/main.tsx
@@ -1,31 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-
-import 'leaflet/dist/leaflet.css'
-import './styles.css'
-
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
 ReactDOM.createRoot(
-  document.getElementById(
-    'root'
-  )!
+  document.getElementById("root")!
 ).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-)
-
-if (
-  'serviceWorker' in navigator &&
-  import.meta.env.PROD
-) {
-  window.addEventListener(
-    'load',
-    () => {
-      navigator.serviceWorker
-        .register('/sw.js')
-        .catch(() => {})
-    }
-  )
-}
+);
